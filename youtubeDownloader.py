b01d9a9e25f7aa5c7d84962349122bfc8f000abd
Oriol Miranda Garrido <113769445+oriolmiranda@users.noreply.github.com>
2023-04-04 17:40:56 +0200

    Update youtubeDownloader.py

--- youtubeDownloader.py
+++ youtubeDownloader.py
@@ -15,4 +15,6 @@ print("Size: ", yt.streams.get_highest_resolution().filesize)
 yd = yt.streams.get_highest_resolution() # Si volem descarregui maxima resolucio
 # yd = yt.streams.get_by_resolution("1080p") # Si volem descarregar una resolucio especifica
 
+# yd = yt.streams.get_audio_only # Descarregar només audio (música)
+
 yd.download(outputFolder)
