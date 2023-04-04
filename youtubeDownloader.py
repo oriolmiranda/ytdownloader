6a5412c5f8b0c57ea9044338f58e887337c4dad0
Oriol Miranda Garrido <113769445+oriolmiranda@users.noreply.github.com>
2023-04-04 17:39:39 +0200

    Initial commit

---
+++ youtubeDownloader.py
@@ -0,0 +1,18 @@
+from pytube import YouTube
+from sys import argv
+
+# Youtube Downloader - al executar el scrip hem de pasar com a parametre el link del video entre cometes ("link")
+
+outputFolder = './VideosDescarregats' # On volem guardar
+
+link = argv[1]
+yt = YouTube(link)
+
+print("Title: ", yt.title)
+print("View: ", yt.views)
+print("Size: ", yt.streams.get_highest_resolution().filesize)
+
+yd = yt.streams.get_highest_resolution() # Si volem descarregui maxima resolucio
+# yd = yt.streams.get_by_resolution("1080p") # Si volem descarregar una resolucio especifica
+
+yd.download(outputFolder)
